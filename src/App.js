3b1e016eeca4d5eaea63d86d548c8a52c40276eb
jpandh <68366181+jpandh@users.noreply.github.com>
2021-04-18 23:14:34 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import logo from './logo.svg';
+import './App.css';
+function snack(){
+  return ("ritz crackers")
+}
+
+function App() {
+  const greeting = "hi";
+
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>{greeting}</h1>
+        <p>
+          {snack()}
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
